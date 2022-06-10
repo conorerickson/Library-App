b44d22cb80626099769f1c985a9b07d8417bafb8
Conor Erickson <89137464+conorerickson@users.noreply.github.com>
2022-06-10 00:40:48 -0400

    update HTML, CSS, JS

---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+let form = document.querySelector('form');
+let titleInp = document.getElementById('title');
+let authorInp = document.getElementById('author');
+let pageInp = document.getElementById('pages');
+let list = document.getElementById('book-list');
+
+function addBook() {
+  let title = titleInp.value;
+  let author = authorInp.value;
+  let pages = pageInp.value;
+  let item =
+  `
+  <thead>
+  <tr>
+    <th>${title}</th>
+    <th>${author}</th>
+    <th>${pages}</th>
+    <th><input type="checkbox" data-toggle="toggle"></th>
+  </tr>
+</thead>
+`;
+  
+  list.insertAdjacentHTML('beforeend', item);
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBook();
+})
